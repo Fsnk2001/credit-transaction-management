937022f9454128e1ff0c99cdb7ebe5c25bf5a79b
Fateme <fsamandarnejad2001@gmail.com>
2024-09-26 22:18:15 +0330

    Add serializer fields and their validations

---
+++ apps/base/validators.py
@@ -0,0 +1,27 @@
+import re
+
+from .exceptions import ValidationError
+
+
+def contains_number_validator(data: str):
+    regex = re.compile('[0-9]')
+    if regex.search(data) == None:
+        raise ValidationError("Input must include number.")
+
+
+def contains_letter_validator(data: str):
+    regex = re.compile('[a-zA-Z]')
+    if regex.search(data) == None:
+        raise ValidationError("Input must include letter.")
+
+
+def contains_special_char_validator(data: str):
+    regex = re.compile('[@_!#$%^&*]')
+    if regex.search(data) == None:
+        raise ValidationError("Input must include special character (@_!#$%^&*).")
+
+
+def phone_number_validator(data: str):
+    regex = re.compile('[^09\d{9}$]')
+    if regex.search(data) == None:
+        raise ValidationError("Invalid phone number.")
