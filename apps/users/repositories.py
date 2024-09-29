3c2f28bad8d601e1fb9f38ab538587e137bb2390
Fateme <fsamandarnejad2001@gmail.com>
2024-09-29 09:21:58 +0330

    Add reset password

--- apps/users/repositories.py
+++ apps/users/repositories.py
@@ -6,3 +6,9 @@ from .serializers import UserSerializer
 class UserRepository(BaseRepository):
     _model = User
     _serializer = UserSerializer
+
+    @classmethod
+    def change_password(cls, instance: User, password: str):
+        instance.set_password(password)
+        instance.save()
+        return instance
--- apps/users/services.py
+++ apps/users/services.py
@@ -4,3 +4,8 @@ from .repositories import UserRepository
 
 class UserService(BaseService):
     _repository = UserRepository
+
+    @classmethod
+    def reset_password(cls, id, password):
+        user = cls.get_by_id(id)
+        return cls._repository.change_password(user, password)
