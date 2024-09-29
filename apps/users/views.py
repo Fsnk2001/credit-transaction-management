b8025d9ab285db511380df3a5f13422c35b569e2
Fateme <fsamandarnejad2001@gmail.com>
2024-09-29 09:22:50 +0330

    Change user view and add reset password

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,7 +5,11 @@ from rest_framework.permissions import IsAuthenticated
 from ..base.views import BaseViewSet
 from ..base.responses import Response
 from ..base.permissions import IsAdminPermission
-from .serializers import UserSerializer
+from .serializers import (
+    UserSerializer,
+    UpdateUserSerializer,
+    ResetPasswordSerializer,
+)
 from .services import UserService
 
 
@@ -18,20 +22,20 @@ class UserViewSet(BaseViewSet):
         self.permission_classes = [IsAdminPermission]
         self.check_permissions(request)
 
-        data = self._service.get_all()
+        users = self._service.get_all()
         return Response(
             data={
-                "users": self.get_serializer(data, many=True).data
+                "users": self.get_serializer(users, many=True).data
             }, message="List of users.", meta={}
         )
 
     @action(detail=False, methods=['get'], url_path='me')
     def get_me(self, request, *args, **kwargs):
         user_id = request.user.id
-        data = self._service.get_by_id(user_id)
+        user = self._service.get_by_id(user_id)
         return Response(
             data={
-                "user": self.get_serializer(data).data
+                "user": self.get_serializer(user).data
             }, message="The user.", meta={}
         )
 
@@ -40,14 +44,17 @@ class UserViewSet(BaseViewSet):
         self.check_permissions(request)
 
         id = kwargs.get('pk')
-        data = self._service.get_by_id(id)
+        user = self._service.get_by_id(id)
         return Response(
             data={
-                "user": self.get_serializer(data).data
+                "user": self.get_serializer(user).data
             }, message="The user.", meta={}
         )
 
     def create(self, request, *args, **kwargs):
+        self.permission_classes = [IsAdminPermission]
+        self.check_permissions(request)
+
         user = self._service.create(request.data)
         return Response(
             data={
@@ -56,10 +63,28 @@ class UserViewSet(BaseViewSet):
         )
 
     def update(self, request, *args, **kwargs):
+        self.permission_classes = [IsAdminPermission]
+        self.check_permissions(request)
+
         id = kwargs.get('pk')
+        serializer = UpdateUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         updated_user = self._service.update(id, request.data)
         return Response(
             data={
                 "user": self.get_serializer(updated_user).data
             }, message="User updated successfully.", status=status.HTTP_200_OK
+        )
+
+    @action(detail=False, methods=['post'], url_path='reset-password')
+    def reset_password(self, request, *args, **kwargs):
+        user_id = request.user.id
+        serializer = ResetPasswordSerializer(data=request.data, context={'user': request.user})
+        serializer.is_valid(raise_exception=True)
+        user = self._service.reset_password(user_id, serializer.validated_data.get('password'))
+        return Response(
+            data={
+                "user": self.get_serializer(user).data
+            }, message="User's password updated successfully.", meta={}
         )
