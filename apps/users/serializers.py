6b8f5bef77151088b0ce2452222eb6e36c3cf488
Fateme <fsamandarnejad2001@gmail.com>
2024-09-29 09:21:37 +0330

    Add create, update user and reset password serializers

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from ..base.serializers import BaseModelSerializer
@@ -8,17 +9,26 @@ from .models import UserRoles, User
 class UserSerializer(BaseModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'is_active', 'password', 'confirm_password')
+        fields = ('id', 'username', 'is_active', 'password', 'confirm_password', 'roles')
         read_only_fields = ('is_superuser', 'is_admin', 'is_seller')
 
     username = serializers.CharField(max_length=255)
     password = PasswordField(min_length=8, write_only=True)
     confirm_password = serializers.CharField(write_only=True)
+    roles = serializers.ListField(child=serializers.CharField(), write_only=True, required=False)
+    is_active = serializers.BooleanField(default=True, required=False)
 
     def validate_username(self, username):
         if User.objects.filter(username=username).exists():
             raise serializers.ValidationError("Username already taken.")
         return username
+
+    def validate_roles(self, value):
+        roles = [choice[0] for choice in UserRoles.choices]
+        invalid_roles = set(value) - set(roles)
+        if invalid_roles:
+            raise serializers.ValidationError(f"Invalid role(s). Allowed roles are: {', '.join(roles)}.")
+        return value
 
     def validate(self, data):
         if not data.get('password') or not data.get('confirm_password'):
@@ -26,6 +36,53 @@ class UserSerializer(BaseModelSerializer):
 
         if data.get('password') != data.get('confirm_password'):
             raise serializers.ValidationError("Confirm password is not equal to password.")
+
+        data.pop('confirm_password', None)
+        return data
+
+    def create(self, validated_data):
+        roles = validated_data.pop('roles', [])
+
+        user = User.objects.create_user(**validated_data)
+        for role in roles:
+            user.add_role(role)
+
+        return user
+
+    def update(self, validated_data):
+        roles = validated_data.pop('roles', [])
+
+        user = User.objects.create_user(**validated_data)
+        for role in roles:
+            user.add_role(role)
+
+        return user
+
+
+class UpdateUserSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=255)
+    roles = serializers.ListField(child=serializers.CharField())
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    old_password = PasswordField()
+    password = PasswordField(min_length=8)
+    confirm_password = serializers.CharField()
+
+    def validate_old_password(self, value):
+        user = self.context['user']
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is not correct.")
+        return value
+
+    def validate(self, data):
+        if data.get('password') != data.get('confirm_password'):
+            raise serializers.ValidationError("Confirm password is not equal to password.")
+
+        try:
+            validate_password(data['password'])
+        except Exception as e:
+            raise serializers.ValidationError("Invalid new password:", e.__str__())
 
         data.pop('confirm_password', None)
         return data
